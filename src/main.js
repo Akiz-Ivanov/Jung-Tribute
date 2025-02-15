8c315d89c7e7928a1874bb05f483f15241206450
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-02-15 08:58:31 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import './style.css'
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const sidebar = document.querySelector(".sidebar");
+const img = document.getElementById("img-div");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+        if (entry.target.id === "img-div") {
+            if (entry.isIntersecting) {
+                sidebar.classList.remove('sidebar-show');
+            } else {
+                sidebar.classList.add('sidebar-show');
+            }
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+observer.observe(img);
