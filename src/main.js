3322821c1e6ea78c0693e4bf2219bda820f0f385
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-06-07 20:59:00 +0200

    feat: add a navigation bar

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-const hiddenElements = document.querySelectorAll('.hidden');
-const sidebar = document.querySelector(".sidebar");
-const img = document.getElementById("img-div");
+// const hiddenElements = document.querySelectorAll('.hidden');
+// const sidebar = document.querySelector(".sidebar");
+// const img = document.getElementById("img-div");
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
 
-        if (entry.target.id === "img-div") {
-            if (entry.isIntersecting) {
-                sidebar.classList.remove('sidebar-show');
-            } else {
-                sidebar.classList.add('sidebar-show');
-            }
-        }
+//         if (entry.target.id === "img-div") {
+//             if (entry.isIntersecting) {
+//                 sidebar.classList.remove('sidebar-show');
+//             } else {
+//                 sidebar.classList.add('sidebar-show');
+//             }
+//         }
 
-    });
+//     });
 
-});
+// });
 
-hiddenElements.forEach((el) => observer.observe(el));
+// hiddenElements.forEach((el) => observer.observe(el));
 
-observer.observe(img);
+// observer.observe(img);
